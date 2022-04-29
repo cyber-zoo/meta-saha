40d6811a4b52348ecd7852308313fe486df0a196
Homalozoa <nx.tardis@gmail.com>
2022-04-29 17:53:34 +0800

    [Modify] Add support for external directory building
    * Now you could building yocto with bitbake anywhere.
    
    Signed-off-by: Homalozoa <nx.tardis@gmail.com>

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
+#
+# Copyright (c) 2022 Homalozoa
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # Initlize Yocto Environment
 #
-GIT_ROOT=$(git rev-parse --show-toplevel | xargs echo -n)
-REPO_PATH=$GIT_ROOT/repos
-RES_PATH=$GIT_ROOT/resources
-LAYER_PATH=$GIT_ROOT/layers
-SCRIPT_PATH=$GIT_ROOT/scripts
+# Directories in Git repo
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+PRJ_ROOT=$SCRIPT_PATH/..
+RES_PATH=$PRJ_ROOT/resources
+#
+# Directories in WorkSpace
+WS_ROOT=$PWD
+REPO_PATH=$WS_ROOT/repos
+LAYER_PATH=$WS_ROOT/layers
 
 if ! command -v vcs &> /dev/null
 then
@@ -36,10 +54,12 @@ if [ -d "$REPO_PATH" ]; then
   if [ ! -e "$LAYER_PATH/.templateconf" ]; then
     touch "$LAYER_PATH/.templateconf"
   fi
+  # link saha-layers
+  ln -fs "$PRJ_ROOT/saha-layers" $WS_ROOT/repos/saha
   for file in $(ls $RES_PATH/link_*)
   do
     while IFS= read -r line; do
-      ln -fs "$GIT_ROOT/$line" $GIT_ROOT/layers/${line##*/}
+      ln -fs "$WS_ROOT/$line" $WS_ROOT/layers/${line##*/}
     done < $file
   done
 fi
