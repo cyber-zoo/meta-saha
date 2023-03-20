c03d424cd14df2b0ab17de57a0cf74036256e843
homalozoa <nx.tardis@gmail.com>
2023-03-20 15:21:21 +0800

    feat:scripts add build.sh
    Signed-off-by: homalozoa <nx.tardis@gmail.com>

---
+++ scripts/build.sh
@@ -0,0 +1,24 @@
+# Copyright (c) 2022 Homalozoa
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Initlize Yocto Environment
+#
+# Directories in Git repo
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+PRJ_ROOT=$SCRIPT_PATH/..
+RES_PATH=$PRJ_ROOT/resources
+MACHINE=$1
+#
+. meta-saha/setup-env --machine $MACHINE
+bitbake saha-image-base
