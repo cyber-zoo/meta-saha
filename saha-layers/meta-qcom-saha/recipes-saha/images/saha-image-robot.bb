1c0bb4307b767b97c98485c8c1afa37d4a7d00c7
Homalozoa <homalozoax@gmail.com>
2026-09-14 15:05:49 +0800

    fix(qcom): correct PSCI reset arguments and debug runtime

--- saha-layers/meta-qcom-saha/recipes-saha/images/saha-image-robot.bb
+++ saha-layers/meta-qcom-saha/recipes-saha/images/saha-image-robot.bb
@@ -8,4 +8,6 @@ CORE_IMAGE_BASE_INSTALL += "packagegroup-saha-ros2"
 # These modules supply the EVK's Ethernet MAC address and Wi-Fi slot power;
 # without them the already packaged network drivers remain probe-deferred.
 CORE_IMAGE_BASE_INSTALL += "kernel-module-at24 kernel-module-pwrseq-pcie-m2"
+# The generic PHY fallback cannot validate this board's 2500BASE-X link.
+CORE_IMAGE_BASE_INSTALL += "kernel-module-qca808x"
 CORE_IMAGE_BASE_INSTALL += "saha-usb-adb"
--- tests/test-build-framework.sh
+++ tests/test-build-framework.sh
@@ -339,7 +339,7 @@ QCOM_TARGET="$ROOT_DIR/kas/targets/iq-9075-evk.yml"
 [ -f "$QCOM_LAYER/conf/distro/saha-qcom.conf" ] || fail "IQ-9075 distro configuration must exist"
 [ -f "$QCOM_LAYER/recipes-saha/images/saha-image-robot.bb" ] || fail "IQ-9075 image recipe must exist"
 QCOM_IMAGE="$QCOM_LAYER/recipes-saha/images/saha-image-robot.bb"
-for package in saha-usb-adb kernel-module-at24 kernel-module-pwrseq-pcie-m2; do
+for package in saha-usb-adb kernel-module-at24 kernel-module-pwrseq-pcie-m2 kernel-module-qca808x; do
   grep -q "$package" "$QCOM_IMAGE" || fail "IQ-9075 bring-up image must include $package"
 done
 grep -qxF 'SAHA_ADB_UDC=a600000.usb' "$QCOM_LAYER/recipes-connectivity/saha-usb-adb/files/saha-usb-adb.conf" ||
@@ -348,7 +348,14 @@ grep -q 'kernel-module-libcomposite kernel-module-usb-f-fs' "$QCOM_LAYER/recipes
   fail "IQ-9075 ADB must package the required gadget modules"
 grep -q 'x-systemd.growfs' "$QCOM_LAYER/recipes-core/base-files/base-files_%.bbappend" ||
   fail "IQ-9075 ext4 must grow to the flashed UFS rootfs partition"
+QCOM_RESET_PATCH="$QCOM_LAYER/recipes-kernel/linux/files/0001-arm64-dts-qcom-lemans-evk-fix-PSCI-reset-arguments.patch"
+grep -q '^+.*mode-edl = <0x1 0>;' "$QCOM_RESET_PATCH" ||
+  fail "IQ-9075 PSCI EDL must encode reset type before cookie"
+grep -q '^+.*mode-bootloader = <0x2 0x10001>;' "$QCOM_RESET_PATCH" ||
+  fail "IQ-9075 PSCI bootloader must encode reset type before cookie"
 ADB_SERVICE="$COMMON_LAYER/recipes-connectivity/saha-usb-adb/files/saha-usb-adb.service"
+grep -qxF 'User=root' "$ADB_SERVICE" ||
+  fail "Saha ADB shells must receive the root login environment for ROS logging"
 grep -qxF 'ExecStartPre=/usr/bin/test -e /dev/usb-ffs/adb/ep0' "$ADB_SERVICE" ||
   fail "Saha ADB must require FunctionFS before starting the daemon"
 grep -qxF 'UnsetEnvironment=ADBD_PORT' "$ADB_SERVICE" ||
